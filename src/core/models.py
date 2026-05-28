0bdff53f2b229b72b01f08b0c21dfe19b5727fdb
Victoria Villarroel <vicvillarroel30@gmail.com>
2026-05-28 18:41:10 -0300

    Agrego los archivos de core

--- src/core/models.py
+++ src/core/models.py
@@ -1,3 +1,19 @@
-from django.db import models
+from django.contrib.auth.decorators import login_not_required  # type:ignore
+from django.urls import reverse_lazy
+from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views.generic.edit import CreateView
 
-# Create your models here.
+from .forms import CustomUserCreationForm
+
+
+@login_not_required
+def home(request):
+    return render(request, "core/index.html")
+
+
+@method_decorator(login_not_required, name="dispatch")
+class RegisterView(CreateView):
+    template_name = "core/register.html"
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy("core:login")
